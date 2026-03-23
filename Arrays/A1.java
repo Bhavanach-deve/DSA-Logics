7d7b087ce35e59fb38c563d05b56eae4d0e3f635
bhavanach-deve <bhavanachilkeshwaram@gmail.com>
2026-03-23 19:40:39 +0530

    Initial DSA commits - Two Sum solution

---
+++ Arrays/A1.java
@@ -0,0 +1,20 @@
+public class A1 {
+    public static int[] twoSum(int[]arr,int target) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] + arr[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+    public static void main(String[] args) {
+        int [] arr={1,2,3,6,4};
+        int target=10;
+        int result[]=twoSum(arr,target);
+        System.out.println("Indices: " + result[0] + ", " + result[1]);
+
+
+    }
+}
