66382f5f01108c61144b32ec118e7aac029ddf7f
bhavanach-deve <bhavanachilkeshwaram@gmail.com>
2026-03-31 18:53:51 +0530

    Add program to find bit at i-th position using left shit

--- BitManipulation/src/FindBitAtIthIndex.java
+++ BitManipulation/src/FindBitAtIthIndex.java
@@ -5,6 +5,14 @@ public class FindBitAtIthIndex
     public static int fetch(int n,int i){
         return (n>>i)&1;
     }
+    public static int fetchBit(int n,int i){
+        int result=n&(1<<i); //moves the value "1" by "i" times to left
+        if(result>0) {
+            return 1;
+        }
+        return 0;
+    }
+
 
     static void main(String[] args) {
       Scanner sc=new Scanner(System.in);
@@ -13,6 +21,7 @@ public class FindBitAtIthIndex
       System.out.println("Enter the index to fetch the value present at:");
       int i=sc.nextInt();
 
-        System.out.println(fetch(n,i));
+        System.out.println("The result when used right shift: "+fetch(n,i));
+        System.out.println("The result when used left shift: "+fetchBit(n,i));
     }
 }
