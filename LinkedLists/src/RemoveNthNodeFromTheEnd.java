4496e07a8aa6a9a602b6982348d6ab5b989f9b13
bhavanach-deve <bhavanachilkeshwaram@gmail.com>
2026-04-16 22:51:28 +0530

    Add program to remove Nth node from the ending

---
+++ LinkedLists/src/RemoveNthNodeFromTheEnd.java
@@ -0,0 +1,48 @@
+class Nnode{
+    int data;
+    Nnode next;
+
+    Nnode(int data){
+        this.data=data;
+        this.next=null;
+    }
+}
+public class RemoveNthNodeFromTheEnd
+{
+    public static Nnode removeEnd(Nnode head,int n){
+        Nnode dummy=new Nnode(0);
+        dummy.next=head;
+        Nnode first=dummy;
+        Nnode second=dummy;
+
+        for(int i=0;i<=n;i++){
+            first=first.next;
+        }
+        while(first!=null){
+            first=first.next;
+            second=second.next;
+        }
+        second.next=second.next.next;
+        return dummy.next;
+
+    }
+    public static void print(Nnode head) {
+        while (head != null) {
+            System.out.print(head.data + "->");
+            head = head.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args)
+    {
+        Nnode head = new Nnode(1);
+        head.next = new Nnode(2);
+        head.next.next = new Nnode(3);
+        head.next.next.next = new Nnode(4);
+        head.next.next.next.next = new Nnode(5);
+        removeEnd(head,3);
+        print(head);
+
+    }
+}
