ad863bdbbda358da93eac9885959bf5f576be4c7
bhavanach-deve <bhavanachilkeshwaram@gmail.com>
2026-04-23 22:46:21 +0530

    Evaluation of postfix expression

---
+++ Stacks/src/EvaluatePostFixExp.java
@@ -0,0 +1,45 @@
+import java.util.Stack;
+
+public class EvaluatePostFixExp
+{
+    public static void main(String[] args)
+    {
+        System.out.println(evaluateExp("2536+**5/2-"));
+    }
+    private static int evaluateExp(String exp){
+        Stack<Integer>st=new Stack<>();
+
+        for(int i=0;i<exp.length();i++){
+          char c=exp.charAt(i);
+
+          if(Character.isDigit(c)){
+              st.push(c-'0');
+          }
+          else{
+              int op2=st.pop();
+              int op1=st.pop();
+
+              switch(c){
+                  case '+':
+                  st.push(op1+op2);
+                  break;
+                  case '-':
+                      st.push(op1-op2);
+                      break;
+                  case '/':
+                      st.push(op1/op2);
+                      break;
+                  case '*':
+                      st.push(op1*op2);
+                      break;
+
+              }
+
+
+
+          }
+        }
+        return st.pop();
+
+    }
+}
