d469e2fd06ba58cdafdba2c76e2670048167e92f
bhavanach-deve <bhavanachilkeshwaram@gmail.com>
2026-04-28 21:02:53 +0530

    Add program to rotate an Array to right by k positions

---
+++ Arrays/Arrays/RightRotateTheArray.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class RightRotateTheArray
+{
+    public static void rightRotate(int arr[],int k) {
+        int n=arr.length;
+        k= k % n;
+        //Copy last k elements
+        int []temp= Arrays.copyOfRange(arr,n-k,n);
+        //Shift the remaining elements to the right
+        for(int i=n-k-1;i>=0;i--){
+            arr[i+k]=arr[i];
+        }
+        //Copy the stored elements to the front
+        for(int i=0;i<k;i++){
+            arr[i]=temp[i];
+        }
+    }
+    public static void main(String[] args)
+    {
+        int arr[]={1,2,3,4,5,6,7};
+        rightRotate(arr,2);
+        System.out.println("Array after right rotation: " + Arrays.toString(arr));
+    }
+}
