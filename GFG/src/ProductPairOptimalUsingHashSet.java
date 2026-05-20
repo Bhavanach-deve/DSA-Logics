26d1360f99782c1be365ef180441712d97db505b
bhavanach-deve <bhavanachilkeshwaram@gmail.com>
2026-05-20 12:48:14 +0530

    Add program to check whether there exists a pair of elements in the array whose product is equal to target-Optimal Solution

---
+++ GFG/src/ProductPairOptimalUsingHashSet.java
@@ -0,0 +1,35 @@
+import java.util.HashSet;
+
+public class ProductPairOptimalUsingHashSet
+{
+    public static boolean productPair(int arr[],int target)
+    {
+        HashSet<Long> set=new HashSet<>();
+        for(int nums:arr)
+        {
+            if(nums==0){
+                if(target==0){
+                    return true;
+                }
+                continue;
+            }
+            if(target%nums==0){
+                long needed=target/nums;
+                if(set.contains(needed)){
+                    return true;
+                }
+            }
+            set.add((long) nums);
+        }
+        return false;
+    }
+
+
+    public static void main(String[] args)
+    {
+        int arr[]={10,20,9,40};
+        int target=400;
+        Boolean status=productPair(arr,target);
+        System.out.println(status);
+    }
+}
