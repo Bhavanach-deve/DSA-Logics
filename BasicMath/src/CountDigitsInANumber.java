48291900d07485865f232b7073947eebcb27a94b
bhavanach-deve <bhavanachilkeshwaram@gmail.com>
2026-05-26 19:01:04 +0530

    Add program count the no.of digits in a number

---
+++ BasicMath/src/CountDigitsInANumber.java
@@ -0,0 +1,17 @@
+public class CountDigitsInANumber
+{
+    public static int countDigits(int n){
+        int count=0;
+        while(n>0){
+            count++;
+            n=n/10;
+        }
+        return count;
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(countDigits(999191918));
+
+    }
+}
