34fdfdc2140c92dbc209851e1011c59c3f91bdb4
bhavanach-deve <bhavanachilkeshwaram@gmail.com>
2026-06-08 17:05:17 +0530

    Add program to find the Bitonic point of an array using Linear Search

--- GFG/src/BitonicPoint.java
+++ GFG/src/BitonicPoint.java
@@ -9,12 +9,26 @@ public class BitonicPoint
         int n=arr.length;
         return arr[n-1];
     }
+    //Linear Search
+    public static int findMax(int arr[])
+    {
+        int max=arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        return max;
+    }
 
    public static void main(String[] args)
    {
        int arr[]={120,100,80,20,0};
        int ans=findMaximum(arr);
        System.out.println(ans);
+       System.out.println("Using Linear Search: ");
+       int lans=findMax(arr);
+       System.out.println(lans);
 
     }
 }
