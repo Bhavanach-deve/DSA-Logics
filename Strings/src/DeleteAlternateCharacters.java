aeaf862c2db94c8ba5492a2ca9ee2afe47016ff6
bhavanach-deve <bhavanachilkeshwaram@gmail.com>
2026-06-17 16:36:40 +0530

    Add program to delete odd-positioned characters

---
+++ Strings/src/DeleteAlternateCharacters.java
@@ -0,0 +1,22 @@
+//Given a string s as input. Delete the characters at odd indices of the string. Return the final string after deletion of characters at odd indices.
+public class DeleteAlternateCharacters
+{
+    public static String delete(String s)
+    {
+        StringBuilder ans=new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            if(i%2==0)
+            {
+                ans.append(s.charAt(i));
+            }
+        }
+        return ans.toString();
+    }
+
+    public static void main(String[] args)
+    {
+        String s="Geeks";
+        System.out.println(delete(s));
+
+    }
+}
